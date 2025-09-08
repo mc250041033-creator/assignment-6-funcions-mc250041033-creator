7de8a7908fbd874df69f6d57fde7172e355324e3
mc250041033-creator <mc250041033@iiti.ac.in>
2025-09-08 22:51:02 +0530

    Update product_except_index.cpp

--- product_except_index.cpp
+++ product_except_index.cpp
@@ -7,6 +7,17 @@ vector<long long> productExceptIndex(const vector<int>& arr) {
     vector<long long> output(size, 1);
 
     // TODO: complete the function as per instructions
+    int prefix = 1;
+    for (int i = 0; i < n; i++;) {
+       output[i] = prefix ;
+        prefix *= arr[i];
+    }
+    int suffix = 1 ;
+    for (int i = n - 1; i >= 1; i--) {
+        output[i] *= suffix;
+        suffix *= arr[i];
+    }
+    return output;
     
 }
 
