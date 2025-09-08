3c38b668e0824e93e9bb37c9e7b4a60270da0c61
mc250041033-creator <mc250041033@iiti.ac.in>
2025-09-08 22:30:59 +0530

    Update prime_palindrome.cpp

--- prime_palindrome.cpp
+++ prime_palindrome.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 bool isPrime(int n) {
     // TODO: complete the function as per instructions
-
+if (n < 2) return false;
+if (n == 2) return true ;
+if (n % 2 == 0) return false;
+    for (int i = 1;i <= sqrt(n); i += 2) {
+ if (n % i == 0 ) return false;        
 }
+return true;
 
 bool isPalindrome(int n) {
     // TODO: complete the function as per instructions
-
+int original = n , reversed = 0;
+ while(n > 0 ) {
+int digit = n % 10 ;
+  reversed = reversed * 10 + digit ;
+  n /= 10;
 }
-
+return original == reversed ;
 bool primePalindrome(int n) {
     // TODO: complete the function as per instructions
-    
+    return isPrime(n) && isPalindrome(n) ;
 }
 
 int main() {
